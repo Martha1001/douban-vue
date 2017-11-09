292a785d713fed9f610d2a7899dcf379866ed4cb
Martha <3032979812@qq.com>
2017-11-09 17:25:43 +0800

    jqku

--- jqku/demo.js
+++ jqku/demo.js
@@ -1,5 +1,7 @@
-var str = 'background:  center center no-repeaturl("img/a22.png")'
-var str1 = "background:  center center no-repeaturl('img/a23.png')"
-// var substr = str.match(/url\('(\S*)'\)|url\("(\S*)"\)/)
-var substr = str.match(/url\((?=\'|\")[\S]*(?=\)'|\)")?/)
+var str = 'background:  center url("img/a212.png")center no-repeat url("img/a22.png")'
+var str1 = "background: url('img/a123.png')  center center no-repeat url('img/a23.png')"
+var substr = str1.match(/url\('(\S*)'\)|url\("(\S*)"\)/g)
+
+// var rex = new RegExp(/url\('(\S*)'\)|url\("(\S*)"\)/g)
+// rex.exec(str)
 console.log(substr)
